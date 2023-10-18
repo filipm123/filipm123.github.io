5fbb24391cc081d9d1ae3bf7a5373ecacc90af5c
Filipor <filipor123@gmail.com>
2023-10-18 15:17:48 +0200

    Initial commit

--- index.js
+++ index.js
@@ -12,31 +12,22 @@ const mediaQuery = window.matchMedia("(max-width:980px)")
 hideSocialsBtn.addEventListener('click', () => {
     socials.style.animation = "fade-out 750ms forwards"
 
-        setTimeout(() => {
-            socials.style.display = 'none'
-        }, 500)
+    setTimeout(() => {
+        socials.style.display = 'none'
+    }, 500)
     hideSocialsBtn.style.display = 'none';
     showSocialsBtn.style.display = 'inline-block'
 })
 
 showSocialsBtn.addEventListener('click', () => {
 
-    
-        socials.style.display = 'inline';
-        socials.style.animation = "fade-in 500ms forwards";
-        showSocialsBtn.style.display='none';
-        hideSocialsBtn.style.display='inline-block'
-    
+
+    socials.style.display = 'inline';
+    socials.style.animation = "fade-in 500ms forwards";
+    showSocialsBtn.style.display = 'none';
+    hideSocialsBtn.style.display = 'inline-block'
+
 })
-
-
-
-
-
-
-
-
-
 
 scrollBtn.addEventListener('click', () => {
 
@@ -78,3 +69,25 @@ scrollToTopBtn.addEventListener('click', () => {
 window.addEventListener("scroll", () => {
     scrollToTopBtn.style.display = window.scrollY > 50 ? 'block' : 'none';
 })
+
+document.querySelector('.contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const form = this;
+    const data = new FormData(form);
+   
+    emailjs.send('service_d1460ha', 'template_w8c8mqo', {
+        to_email: data.get('email'),
+        from_name:data.get('name'),
+        message : data.get('message'),
+    }).then(function(response) {
+        console.log('Email sent:', response);
+        alert('email sent')
+        form.reset();
+        
+    }), function(error){
+        console.log('Email error', error);
+        alert('email not sent')
+        form.reset();
+    }
+  
+});
